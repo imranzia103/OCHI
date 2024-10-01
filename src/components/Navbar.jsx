08dcb85c404036cb07a4b77f71a6e4be389dbcea
imranzia103 <158277003+imranzia103@users.noreply.github.com>
2024-10-01 07:07:51 +0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full px-20 py-8 font-['Neue Montreal']">
+      <div className="logo"></div>
+    </div>
+  );
+};
+
+export default Navbar;
